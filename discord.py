c5f18486d3b3755a02d5e4cb233758a9c37d4acc
Abhishek35411 <168454831+Abhishek35411@users.noreply.github.com>
2025-01-13 08:46:29 +0530

    Create discord.py

---
+++ discord.py
@@ -0,0 +1,25 @@
+
+
+import discord
+import google.generativeai as genai
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+        if self.user!= message.author:
+            if self.user in message.mentions:
+                channel= message.channel
+                genai.configure(api_key="secret_key") 
+                model = genai.GenerativeModel("gemini-1.5-flash")
+                response = model.generate_content(f'{message.content}')
+                messageToSend = response.text
+                await channel.send(messageToSend)
+        
+intents = discord.Intents.default()
+intents.message_content = True
+client = MyClient(intents=intents)
+client.run('token')
